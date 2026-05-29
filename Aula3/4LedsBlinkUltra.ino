a654d5168580d219dfd1d8198a1fa2a5cf20c708
JohneSs14 <130942863+JohneSs14@users.noreply.github.com>
2026-05-29 09:38:58 -0300

    new 4LedsBlink code

---
+++ Aula3/4LedsBlinkUltra.ino
@@ -0,0 +1,19 @@
+const int LEDS[4] = {26, 27, 32, 33};
+
+void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(LEDS[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(LEDS[i], HIGH);
+    if (i == 0) {
+      digitalWrite(LEDS[3], LOW);
+    } else {
+      digitalWrite(LEDS[i-1], LOW);
+    }
+    delay(500);
+  }
+}
