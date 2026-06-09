920d38eca7ac5e7c7ecb2cd43989b8d1292614fe
JohneSs14 <130942863+JohneSs14@users.noreply.github.com>
2026-06-09 15:35:01 -0300

    Refactor ServoTeste by removing Serial prints
    Removed serial communication for debugging and simplified setup.

--- Aula5/ServoTeste.ino
+++ Aula5/ServoTeste.ino
@@ -2,7 +2,7 @@
 
 const int SERVO_PIN = 5;
 const int SERVO_FREQ = 50;
-const int SERVO_RES = 14; 
+const int SERVO_RES = 14;
 
 uint32_t anguloParaDuty(int angulo) {
   int pulsoUs = map(angulo, 0, 180, 500, 2500);
@@ -14,26 +14,19 @@ void moverServo(int angulo) {
   if (angulo > 180) angulo = 180;
 
   ledcWrite(SERVO_PIN, anguloParaDuty(angulo));
-
-  Serial.print("Servo angulo=");
-  Serial.println(angulo);
 }
 
 void setup() {
-  Serial.begin(115200);
-
-  if (!ledcAttach(SERVO_PIN, SERVO_FREQ, SERVO_RES)) {
-    Serial.println("ERRO: ledcAttach falhou. Confira GPIO e resolucao.");
-  } else {
-    Serial.println("ServoTeste: 0 -> 90 -> 180");
-  }
+  ledcAttach(SERVO_PIN, SERVO_FREQ, SERVO_RES);
 }
 
 void loop() {
   moverServo(0);
   delay(1000);
+
   moverServo(90);
   delay(1000);
+
   moverServo(180);
   delay(1000);
 }
