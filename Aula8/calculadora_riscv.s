d4f6ae754d4c4f7c7b2fba90cc99a46756d81a04
JohneSs14 <130942863+JohneSs14@users.noreply.github.com>
2026-06-30 19:03:51 -0300

    Refactor RISC-V calculator assembly code

--- Aula8/calculadora_riscv.s
+++ Aula8/calculadora_riscv.s
@@ -1,53 +1,68 @@
 # calculadora_riscv.s
 
-.globl soma
-.globl subtracao
-.globl multiplicacao
-.globl fatorial
+    .text
 
-###################################################
-# int soma(int a,int b)
+    .global _start
+    .global soma
+    .global subtracao
+    .global multiplicacao
+    .global fatorial
+
+#--------------------------------------------------
+# Ponto de entrada (teste)
+#--------------------------------------------------
+_start:
+    # Teste: soma(10, 20)
+    li a0, 10
+    li a1, 20
+    call soma
+
+    # Resultado fica em a0
+
+fim:
+    j fim
+
+#--------------------------------------------------
+# int soma(int a, int b)
 # a -> a0
 # b -> a1
-###################################################
+# retorno -> a0
+#--------------------------------------------------
 soma:
     add a0, a0, a1
     ret
 
-###################################################
-# int subtracao(int a,int b)
-###################################################
+#--------------------------------------------------
+# int subtracao(int a, int b)
+#--------------------------------------------------
 subtracao:
     sub a0, a0, a1
     ret
 
-###################################################
-# int multiplicacao(int a,int b)
-###################################################
+#--------------------------------------------------
+# int multiplicacao(int a, int b)
+#--------------------------------------------------
 multiplicacao:
     mul a0, a0, a1
     ret
 
-###################################################
+#--------------------------------------------------
 # int fatorial(int n)
-###################################################
+# n -> a0
+# retorno -> a0
+#--------------------------------------------------
 fatorial:
-
     li t0, 1          # resultado
     li t1, 2          # i
 
 loop:
-
-    bgt t1, a0, fim
+    bgt t1, a0, fim_fat
 
     mul t0, t0, t1
-
     addi t1, t1, 1
 
     j loop
 
-fim:
-
-    mv a0, t0
-
+fim_fat:
+    mv a0, t0         # retorna resultado em a0
     ret
