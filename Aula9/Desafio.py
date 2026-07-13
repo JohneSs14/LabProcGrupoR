a0672cb3689c276f12ee9de5067776517806ed63
JohneSs14 <130942863+JohneSs14@users.noreply.github.com>
2026-07-13 16:17:57 -0300

    Add BPM control with servo and buzzer functionality
    Implement a BPM control system using buttons to adjust the speed of a servo and buzzer.

---
+++ Aula9/Desafio.py
@@ -0,0 +1,38 @@
+from gpiozero import Button
+from gpiozero import AngularServo
+from gpiozero import TonalBuzzer
+from gpiozero.tones import Tone
+from time import sleep
+import time
+
+servo = AngularServo(17)
+buzzer = TonalBuzzer(23)
+
+mais = Button(24)
+menos = Button(25)
+
+bpm = 60
+
+estado = False
+
+while True:
+
+    if mais.is_pressed:
+        bpm += 5
+        sleep(0.2)
+
+    if menos.is_pressed:
+        bpm = max(20,bpm-5)
+        sleep(0.2)
+
+    periodo = 60/bpm
+
+    servo.angle = 45 if estado else -45
+
+    buzzer.play(Tone("A5"))
+    sleep(0.08)
+    buzzer.stop()
+
+    estado = not estado
+
+    sleep(periodo-0.08)
