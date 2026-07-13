14a36e83ccf51990dd179c7d691ef7e1c30789c7
JohneSs14 <130942863+JohneSs14@users.noreply.github.com>
2026-07-13 16:17:27 -0300

    Add RaspMetronomo.py for controlling servo and buzzer

---
+++ Aula9/RaspMetronomo.py
@@ -0,0 +1,25 @@
+from gpiozero import AngularServo
+from gpiozero import TonalBuzzer
+from gpiozero.tones import Tone
+from time import sleep
+
+servo = AngularServo(17)
+buzzer = TonalBuzzer(23)
+
+estado = False
+
+while True:
+
+    if estado:
+        servo.angle = -45
+    else:
+        servo.angle = 45
+
+    buzzer.play(Tone("A5"))
+    sleep(0.10)
+
+    buzzer.stop()
+
+    estado = not estado
+
+    sleep(0.90)
